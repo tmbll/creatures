8ef8839dbd89574016aa3d2114429d9269e4a86a
tmbll <bull.tab@gmail.com>
2023-07-31 17:09:34 +0100

    Add Battle class

--- test/battle.test.ts
+++ test/battle.test.ts
@@ -10,3 +10,13 @@ describe("Battle", () => {
     expect(battle.creature2).toBe(creature2);
   });
 });
+
+class Battle {
+  creature1: Creature;
+  creature2: Creature;
+
+  constructor(creature1: Creature, creature2: Creature) {
+    this.creature1 = creature1;
+    this.creature2 = creature2;
+  }
+}
