0220166299d09b6e0b1d0ab2b5260aaf146742c7
tmbll <bull.tab@gmail.com>
2023-07-31 17:02:43 +0100

    Add HP and CP to Creature

--- src/Creature.ts
+++ src/Creature.ts
@@ -1,9 +1,13 @@
 import { Entity } from "./Entity";
 
+const randomNumberBetween1And10 = Math.floor(Math.random() * 10) + 1;
+
 export class Creature implements Entity {
   species: string;
   family: string;
   position: { x: number; y: number };
+  HP: number = randomNumberBetween1And10;
+  CP: number = randomNumberBetween1And10;
 
   constructor(
     species: string,
