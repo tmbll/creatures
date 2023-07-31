866807e1ff9be96e3d7f91280e8c9a7ecdab82f9
tmbll <bull.tab@gmail.com>
2023-07-31 17:32:18 +0100

    Implement fight method

--- src/Battle.ts
+++ src/Battle.ts
@@ -8,4 +8,19 @@ export class Battle {
     this.creature1 = creature1;
     this.creature2 = creature2;
   }
+
+  fight() {
+    const fighters = [this.creature1, this.creature2].sort(
+      () => Math.random() - 0.5
+    );
+    let attacker = fighters[0];
+    let defender = fighters[1];
+
+    while (this.creature1.HP > 0 && this.creature2.HP > 0) {
+      defender.takeDamage(attacker.CP);
+      [attacker, defender] = [defender, attacker]; // Swap roles
+    }
+
+    return this.creature1.HP > 0 ? this.creature1 : this.creature2;
+  }
 }
