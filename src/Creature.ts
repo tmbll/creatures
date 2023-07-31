7a583a5ce051bd02631ac6ed1bd5c838da9344d4
tmbll <bull.tab@gmail.com>
2023-07-31 17:25:36 +0100

    Optional values of HP and CP to Creature

--- src/Creature.ts
+++ src/Creature.ts
@@ -1,22 +1,30 @@
 import { Entity } from "./Entity";
 
-const randomNumberBetween1And10 = Math.floor(Math.random() * 10) + 1;
+const randomNumberBetween1And10 = () => Math.floor(Math.random() * 10) + 1;
 
 export class Creature implements Entity {
   species: string;
   family: string;
   position: { x: number; y: number };
-  HP: number = randomNumberBetween1And10;
-  CP: number = randomNumberBetween1And10;
+  HP: number;
+  CP: number;
 
   constructor(
     species: string,
     family: string,
-    position: { x: number; y: number }
+    position: { x: number; y: number },
+    HP?: number,
+    CP?: number
   ) {
     this.species = species;
     this.family = family;
     this.position = position;
+
+    // If HP is provided and not null, use the provided value; otherwise, use random number
+    this.HP = HP ?? randomNumberBetween1And10();
+
+    // If CP is provided and not null, use the provided value; otherwise, use random number
+    this.CP = CP ?? randomNumberBetween1And10();
   }
 
   takeDamage(damage: number) {
