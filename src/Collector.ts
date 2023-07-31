23a9ebcbf6f31da692ae04046db2dd458df20ab3
tmbll <bull.tab@gmail.com>
2023-07-31 15:52:40 +0100

    Catch implemented

--- src/Collector.ts
+++ src/Collector.ts
@@ -1,9 +1,15 @@
+import { Creature } from "./Creature";
 import { Entity } from "./Entity";
 
 export class Collector implements Entity {
   position: { x: number; y: number };
+  collection: Creature[] = [];
 
   constructor(position: { x: number; y: number }) {
     this.position = position;
+  }
+
+  catch(creature: Creature) {
+    this.collection.push(creature);
   }
 }
